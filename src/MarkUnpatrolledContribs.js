d5b77b7b399547ce296b5639e96285d67bd1e47e
Helder <he7d3r@gmail.com>
2014-09-20 11:22:28 -0300

    Remove tracking link (only useful on wiki)

--- src/MarkUnpatrolledContribs.js
+++ src/MarkUnpatrolledContribs.js
@@ -2,7 +2,6 @@
  * Mark unpatrolled contributions of a given user with "!" on [[Special:Contributions]] and the history of pages
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/MarkUnpatrolledContribs.js]] ([[File:User:Helder.wiki/Tools/MarkUnpatrolledContribs.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, onevar: true */
 /*global jQuery, mediaWiki */
