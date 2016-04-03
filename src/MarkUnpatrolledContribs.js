f46d79241517a76d3d0a668ff8cd16723db28df9
Helder <he7d3r@gmail.com>
2016-04-02 21:52:32 -0300

    Add continuation to API query; reorganize

--- src/MarkUnpatrolledContribs.js
+++ src/MarkUnpatrolledContribs.js
@@ -10,50 +10,61 @@
 		// [[MediaWiki:Recentchanges-label-unpatrolled]]
 		'muc-recentchanges-label-unpatrolled': 'Esta edição ainda não foi patrulhada'
 	} );
+	function mark( revids ) {
+		var $marker = $( '<abbr class="unpatrolled">!</abbr>' )
+			.attr( 'title', mw.msg( 'muc-recentchanges-label-unpatrolled' ) );
+		$( '#mw-content-text' ).find( 'li' ).each( function () {
+			var i,
+				$this = $( this ),
+				href = $this.find( 'a.mw-changeslist-date' ).attr( 'href' );
+			if ( !href ) {
+				return true;
+			}
+			for ( i = 0; i < revids.length; i++ ) {
+				if ( href.indexOf( 'oldid=' + revids[ i ] ) !== -1 ) {
+					$this.prepend( $marker.clone(), ' ' )
+						.addClass( 'not-patrolled' );
+				}
+			}
+		} );
+	}
 
 	function markUnpatrolledContribs() {
-		var param = {
-			action: 'query',
-			list: 'recentchanges',
-			rcprop: 'timestamp|title|ids|patrolled',
-			rclimit: 'max'
-		};
+		var api = new mw.Api(),
+			param = {
+				action: 'query',
+				list: 'recentchanges',
+				rcprop: 'timestamp|title|ids|patrolled',
+				rclimit: 'max',
+				formatversion: 2,
+				'continue': ''
+			},
+			getRC = function ( queryContinue ) {
+				$.extend( param, queryContinue );
+				api.get( param ).done( function ( data ) {
+					var unpatrolled = [],
+						pg = mw.config.get( 'wgPageName' ).replace( /_/g, ' ' );
+					$.each( data.query.recentchanges, function () {
+						if ( this.patrolled ||
+							( mw.config.get( 'wgAction' ) === 'history' && this.title !== pg )
+						) {
+							// Continue
+							return true;
+						}
+						unpatrolled.push( this.revid );
+					} );
+					if ( unpatrolled.length ) {
+						mark( unpatrolled );
+					}
+					if ( data[ 'continue' ] ) {
+						getRC( data[ 'continue' ] );
+					}
+				} );
+			};
 		if ( mw.config.get( 'wgAction' ) !== 'history' ) {
 			param.rcuser = mw.config.get( 'wgRelevantUserName' );
 		}
-		( new mw.Api() ).get( param ).done( function ( data ) {
-			var unpatrolled = [],
-				$marker,
-				pg = mw.config.get( 'wgPageName' ).replace( /_/g, ' ' );
-			$.each( data.query.recentchanges, function () {
-				if ( this.unpatrolled !== '' ||
-					( mw.config.get( 'wgAction' ) === 'history' && this.title !== pg )
-				) {
-					// Continue
-					return true;
-				}
-				unpatrolled.push( this.revid );
-			} );
-			if ( unpatrolled.length === 0 ) {
-				return;
-			}
-			$marker = $( '<abbr class="unpatrolled">!</abbr>' )
-				.attr( 'title', mw.msg( 'muc-recentchanges-label-unpatrolled' ) );
-			$( '#mw-content-text' ).find( 'li' ).each( function () {
-				var i,
-					$this = $( this ),
-					href = $this.find( 'a.mw-changeslist-date' ).attr( 'href' );
-				if ( !href ) {
-					return true;
-				}
-				for ( i = 0; i < unpatrolled.length; i++ ) {
-					if ( href.indexOf( 'oldid=' + unpatrolled[ i ] ) !== -1 ) {
-						$this.prepend( $marker.clone(), ' ' )
-							.addClass( 'not-patrolled' );
-					}
-				}
-			} );
-		} );
+		getRC();
 	}
 
 	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Contributions' || mw.config.get( 'wgAction' ) === 'history' ) {
